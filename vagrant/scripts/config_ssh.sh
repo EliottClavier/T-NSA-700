bbc054d871df4144b3b4888e8e04f4f7164f6851
Clemathe <clement.mathe@rdtingenieurs.fr>
2023-02-06 12:13:23 +0100

    [NSA-64] variabilisation

--- vagrant/scripts/config_ssh.sh
+++ vagrant/scripts/config_ssh.sh
@@ -1,19 +1,3 @@
-#if [ -z "$(ls -A /home/vagrant/.ssh)" ]; then
-
-#   sudo mkdir -p /home/vagrant/.ssh
-#   sudo touch /home/vagrant/.ssh/known_hosts
-#   sudo chmod 700 /home/vagrant/.ssh
-
-#fi
-
-# add web/database hosts to known_hosts (IP is defined in Vagrantfile)
-#sudo ssh-keyscan -H 192.168.153.48 >> /home/vagrant/.ssh/known_hosts
-#sudo ssh-keyscan -H 192.168.153.49 >> /home/vagrant/.ssh/known_hosts
-#sudo ssh-keyscan -H 192.168.153.78 >> /home/vagrant/.ssh/known_hosts
-#sudo chown vagrant:vagrant /home/vagrant/.ssh/known_hosts
-
-# reload ssh in order to load the known hosts
-#/etc/init.d/ssh reload
 
 # Activate Password Authentication
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
